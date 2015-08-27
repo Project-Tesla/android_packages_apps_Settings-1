53e1e031608b10d4f16fd0b196089841a3df1617
NoelMacwan <yulespencer@gmail.com>
2015-08-27 17:04:01 +0530

    Tesla Settings: add Team Tesla

---
+++ src/com/android/settings/tesla/TeamTesla.java
@@ -0,0 +1,97 @@
+package com.android.settings.tesla;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+
+
+public class TeamTesla extends Activity {
+    private EditText field;
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        field = (EditText) findViewById(R.id.editText);
+        webView = (WebView) findViewById(R.id.webView1);
+        webView.setWebViewClient(new MyBrowser());
+//
+        View view = getLayoutInflater().inflate(R.layout.actionbar, null);
+        getActionBar().setDisplayShowCustomEnabled(true);
+        getActionBar().setCustomView(view);
+        getActionBar().getCustomView().findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open(((EditText) getActionBar().getCustomView().findViewById(R.id.editText)).getText().toString());
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
+    public void open(String url) {
+            url = "http://teslaos.github.io/team.html";
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.loadUrl(url);
+
+    }
+
+    private class MyBrowser extends WebViewClient {
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            ((EditText) getActionBar().getCustomView().findViewById(R.id.editText)).setText(url);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        } else if (id == android.R.id.home) {
+            if (webView.canGoBack()) {
+                webView.goBack();
+            } else {
+                this.finish();
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
